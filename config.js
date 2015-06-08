5bd0099dac6d2dbdad9afe7113c3334a04f8b060
psyomn <lethaljellybean@gmail.com>
2015-06-08 02:09:19 -0400

    config.js: explain jargon

--- config.js
+++ config.js
@@ -10,8 +10,32 @@ var fs        = require('fs');
 var colors    = require('colors');
 var util      = require('util');
 var sprintf   = require('sprintf-js').sprintf;
+
 /*
- * This should take care setting information about the overall project
+ * This should take care setting information about the overall project.
+ *
+ * Let's get some jargon out of the way:
+ *   - datbase: in our context are json files. A database file contains data in
+ *       json format. For the time being, we have a list of monsters in this
+ *       format. An example format is the following:
+ *
+ *         {
+ *           "gobling": {
+ *             "hp": 12,
+ *             "attack": "12d2",
+ *             ...
+ *           },
+ *           "imp": {
+ *             ...
+ *           }
+ *         }
+ *
+ *  - schema: describes what fields the data should have. We use the schema for
+ *      two things: 1) we use it to create new entities interactively. 2) we can
+ *      use the schema in order to filter out extra fields. This way, different
+ *      monsters could be shared between games (this is a nice to have at the
+ *      moment).
+ *
  */
 
 function setDataSource(index) {
@@ -32,22 +56,35 @@ function setAttribute(label, value) {
   writeConfiguration(prevConfig);
 }
 
+/**
+ * @return String the filename of the data source file
+ */
 function getDataSource() {
   var config = readConfiguration();
   return config["datasource"];
 }
 
+/**
+ * @return String the filename of the schema
+ */
 function getSchemaSource() {
   var config = readConfiguration();
   return config["schemasource"];
 }
 
+/**
+ * list all the files which are databases
+ */
 function lsDataSources() {
   var ext = constants.DB_EXTENSION;
   var chosen = getDataSource();
   lsPretty(ext, chosen, "database");
 }
 
+/**
+ * list things in a pretty fashion (to be used with extensions). For example,
+ * this function may be reused for schemas, or even database files.
+ */
 function lsPretty(ext, usedFilename, label) {
   var message = "Available " + label + "(s):";
   console.log(message.green);
@@ -98,13 +135,7 @@ function writeConfiguration(json) {
   fs.writeFileSync(file, JSON.stringify(json));
 }
 
-function maybeCreateWrite(absPath, contents) {
-  var fd;
-  fd = fs.openSync(absPath);
-  fs.writeSync(fd, contents);
-  fs.closeSync(fd);
-}
-
 function readSchema() {
+  var filename = getSchemaSource();
 }
 
